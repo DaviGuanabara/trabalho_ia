b5d89df6814a4081adf4e85519a9ee2f29acd78b
Andrey Labanca <andrey.labanca@ga.ita.br>
2022-06-25 01:17:35 -0300

    commit Andrey

--- supermario/callbacks.py
+++ supermario/callbacks.py
@@ -36,23 +36,24 @@ class SaveOnBestTrainingRewardCallback(BaseCallback):
         #    os.makedirs(self.save_path, exist_ok=True)
 
     def _on_step(self) -> bool:
+
         if self.n_calls % self.check_freq == 0:
 
-          # Retrieve training reward
-          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
-          if len(x) > 0:
-              # Mean training reward over the last 100 episodes
-              mean_reward = np.mean(y[-100:])
-              #if self.verbose > 0:
-                #print(f"Num timesteps: {self.num_timesteps}")
-                #print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
+            # Retrieve training reward
+            x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+            if len(x) > 0:
+                # Mean training reward over the last 100 episodes
+                mean_reward = np.mean(y[-100:])
+                if self.verbose > 0:
+                    print(f"Num timesteps: {self.num_timesteps}")
+                    print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
 
-              # New best model, you could save the agent here
-              if mean_reward > self.best_mean_reward:
-                  self.best_mean_reward = mean_reward
-                  # Example for saving best model
-                  if self.verbose > 0:
-                    print(f"Saving new best model to {self.save_path}")
-                  self.model.save(self.save_path)
+                # New best model, you could save the agent here
+                if mean_reward > self.best_mean_reward:
+                    self.best_mean_reward = mean_reward
+                    # Example for saving best model
+                    if self.verbose > 0:
+                        print(f"Saving new best model to {self.save_path}")
+                        self.model.save(self.save_path)
 
         return True
